3f013fb559c289b0e876d2155ab66ddaedb39f20
Irina Guberman <irina.guberman@gmail.com>
2017-11-05 17:50:14 -0600

    minimize mnesia use and get session info by creating session-long processes

--- src/oneup.erl
+++ src/oneup.erl
@@ -81,17 +81,22 @@ infinite_create_oneup() ->
 % functions.
 
 init() ->
-    SoName = 
-        case code:priv_dir(?APPNAME) of
-            {error, bad_name} ->
-                case filelib:is_dir(filename:join(["..", priv])) of
-                    true ->
-                        filename:join(["..", priv, ?LIBNAME]);
-                    _ ->
-                        filename:join([priv, ?LIBNAME])
-                end;
-            Dir ->
-                filename:join(Dir, ?LIBNAME)
+    SoName =
+        case filelib:find_file("oneup.so", "_build/default/lib/oneup/priv") of
+            {ok, SoNameFull} -> [SoNamePrefix, _Rest] = string:split(SoNameFull, ".so"),
+                SoNamePrefix;
+            _Other ->
+                case code:priv_dir(?APPNAME) of
+                    {error, bad_name} ->
+                        case filelib:is_dir(filename:join(["..", priv])) of
+                            true ->
+                                filename:join(["..", priv, ?LIBNAME]);
+                            _ ->
+                                filename:join([priv, ?LIBNAME])
+                        end;
+                    Dir ->
+                        filename:join(Dir, ?LIBNAME)
+                end
         end,
     erlang:load_nif(SoName, 0).
 
